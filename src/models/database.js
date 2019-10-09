909470a5b2e30770947e4197cc24ce5207b089c4
superyaz <yacamocursos@hotmail.com>
2019-10-08 21:45:47 -0500

    Se modifica el archivo database para el tipo de conexion

--- src/models/database.js
+++ src/models/database.js
@@ -1,8 +1,19 @@
-const environment = process.env.NODE_ENV || "dev";
-const config = require('../config/database.json')[environment];
+// const environment = process.env.NODE_ENV || "dev";
+// const config = require('../config/database.json')[environment];
+// const mongoose = require('mongoose');
+
+
+// mongoose.connect(...config)
+//     .then(() => console.log('La base de datos esta conectada'))
+//     .catch(() => console.log('Error al conectar la base de datos'));
+
 const mongoose = require('mongoose');
 
-
-mongoose.connect(...config)
-    .then(() => console.log('La base de datos esta conectada'))
-    .catch(() => console.log('Error al conectar la base de datos'));
+mongoose.set('useFindAndModify', false);
+mongoose.connect('mongodb+srv://yacamo:yaz.456@cluster0-5xtyo.mongodb.net/kewars?retryWrites=true&w=majority', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(db => console.log('DB is connected'))
+    .catch(err => console.error(err));
